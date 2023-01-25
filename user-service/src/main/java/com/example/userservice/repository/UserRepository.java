6081651511f9993f1eb53908aff68dd24b9ff9c1
Muharrem Koç <80245013+muharremkoc@users.noreply.github.com>
2023-01-25 22:49:24 +0300

    Spring-Feign-Resilience4j-Different-Method

--- user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.userservice.repository;
-
-import com.example.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
