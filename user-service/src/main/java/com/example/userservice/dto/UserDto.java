0648705142d8f6d7016ff2ff31567587ad6e8897
Muharrem Koç <80245013+muharremkoc@users.noreply.github.com>
2023-01-25 22:50:23 +0300

    Spring-Feign-Resilience4j-Different-Method

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.example.userservice.dto;
+
+public class UserDto {
+
+    private String name;
+
+    public UserDto() {
+    }
+
+    public UserDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
